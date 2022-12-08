4066c0d0747df20ac50a0573d45f568d53c2dcf3
Patrick Gilmartin <patrickdgilmartin@gmail.com>
2022-12-08 02:46:41 -0700

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '<router-outlet></router-outlet>',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tortoise';
+}
